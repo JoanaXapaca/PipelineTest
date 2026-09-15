4a8da04e44aeaf2f00394898688c22fc04935b92
JoanaXapaca <148261838+JoanaXapaca@users.noreply.github.com>
2026-09-15 15:57:02 +0000

    Update coverage configuration in vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default mergeConfig(
       root: fileURLToPath(new URL('./', import.meta.url)),
       coverage: {
         provider: 'v8',
-        experimentalAstAwareRemapping: true,
+        ignoreEmptyLines: false,
       },
     },
   }),
